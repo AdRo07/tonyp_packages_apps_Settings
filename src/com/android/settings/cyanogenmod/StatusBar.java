5f727d1f97f339d165b347611d15e3e46cb296bd
Peter Gregus <pete.gregus@gmail.com>
2013-01-19 21:23:07 +0100

    Option to disable low battery warning and sound (2/2)
    Change-Id: Id2bef8f0044ca8adaf2eed8df71eefbbc1240519
    
    Conflicts:
    	res/values/strings.xml
    	src/com/android/settings/cyanogenmod/StatusBar.java

--- src/com/android/settings/cyanogenmod/StatusBar.java
+++ src/com/android/settings/cyanogenmod/StatusBar.java
@@ -53,6 +53,8 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private static final String STATUS_BAR_SHOW_DAYMONTH = "status_bar_show_daymonth";
     private static final String STATUS_BAR_DAYMONTH_SIZE = "status_bar_daymonth_size";
     private static final String STATUS_BAR_COLOR = "status_bar_color";
+    private static final String PREF_LOW_BATTERY_WARNING = "pref_low_battery_warning";
+    private static final String PREF_LOW_BATTERY_SOUND = "pref_low_battery_sound";
 
     private ListPreference mStatusBarAmPmSize;
     private ListPreference mStatusBarBattery;
@@ -72,6 +74,8 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private CheckBoxPreference mStatusBarDoNotDisturb;
     private PreferenceCategory mPrefCategoryGeneral;
     private PreferenceCategory mPrefCategoryClock;
+    private CheckBoxPreference mLowBatteryWarning;
+    private CheckBoxPreference mLowBatterySound;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -101,6 +105,8 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
         mStatusBarColor = (ColorPickerPreference) prefSet.findPreference(STATUS_BAR_COLOR);
         mStatusBarColor.setOnPreferenceChangeListener(this);
         mStatusBarColor.setAlphaSliderEnabled(true);
+        mLowBatteryWarning = (CheckBoxPreference) prefSet.findPreference(PREF_LOW_BATTERY_WARNING);
+        mLowBatterySound = (CheckBoxPreference) prefSet.findPreference(PREF_LOW_BATTERY_SOUND);
 
         mStatusBarClock.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.STATUS_BAR_CLOCK, 1) == 1));
@@ -120,6 +126,11 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
                 Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0) == 1));
         mStatusBarDoNotDisturb.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.STATUS_BAR_DONOTDISTURB, 0) == 1));
+        mLowBatteryWarning.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.POWER_UI_LOW_BATTERY_WARNING, 1) == 1));
+        mLowBatterySound.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.POWER_UI_LOW_BATTERY_SOUND, 1) == 1));
+        mLowBatterySound.setEnabled(mLowBatteryWarning.isChecked());
 
         try {
             if (Settings.System.getInt(getActivity().getContentResolver(),
@@ -314,6 +325,17 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
             value = mStatusBarNotifCount.isChecked();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUS_BAR_NOTIF_COUNT, value ? 1 : 0);
+            return true;
+        } else if (preference == mLowBatteryWarning) {
+            value = mLowBatteryWarning.isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_UI_LOW_BATTERY_WARNING, value ? 1 : 0);
+            mLowBatterySound.setEnabled(value);
+            return true;
+        } else if (preference == mLowBatterySound) {
+            value = mLowBatterySound.isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_UI_LOW_BATTERY_SOUND, value ? 1 : 0);
             return true;
         }
         return false;
